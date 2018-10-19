ee1449f9306c34ddd8a60637e9d8db2db1ec0c89
AIM1993 <244018719@qq.com>
2018-10-19 23:42:01 +0800

    解决页码功能中

--- djangoshop/djangoshop/store/views.py
+++ djangoshop/djangoshop/store/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from django.conf import settings
 from .models import Product, Category
-from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger 
+from django.core.paginator import Paginator
 # Create your views here.
 
 
-def get_page(request, product_list):
-    paginator = Paginator(product_list, 2)
-    try:
-        page = int(request.GET.get('page', 1))
-        product_list = paginator.page(page)
-    except (EmptyPage, PageNotAnInteger, InvalidPage):
-        product_list = paginator.page(1)
-    return product_list
+def get_page(request, product_all_list):
+    paginator = Paginator(product_all_list, 3)
+
+    page_num = request.GET.get('page', 1)
+    page_of_products = paginator.get_page(page_num)
+    current_page_num = page_of_products.number
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + list(range(current_page_num, min(current_page_num + 2, paginator.num_pages + 1)))
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, "...")
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append("...")
+    if page_range[0] != 1:
+        page_range.insert(0, 1) 
+        
+    context = {}
+    context['blogs'] = page_of_products.object_list
+    context['page_of_products'] = page_of_products
+    context['page_range'] = page_range
+
+    return context
 
 
 def index(request):
@@ -29,11 +41,13 @@ def product(request):
 
 
 def store(request):
+    product_all_list = Product.objects.all()
+    product_page = get_page(request, product_all_list)
     categories_list = Category.objects.all()
     product_list = Product.objects.all()
-    new_product_list = Product.new_product.all()
-    hot_product_list = Product.hot_product.all()
+
     return render(request, "store.html", locals())
+
 
 
 def about(request):
